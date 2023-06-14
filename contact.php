5df32e8f8e7851473450f15302b9cc2ec90a209f
Gkilgore <gkilgore@frhsd.com>
2023-06-14 00:27:17 -0400

    changed email addresses on form

--- contact.php
+++ contact.php
@@ -1,24 +1,24 @@
 
 <?php
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'kilgore@dhacontracting.com';
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'Dylan@dhacontracting.com';
 
-  if (file_exists($php_email_form = 'php-email-form.php')) {
-    include($php_email_form);
-  } else {
-    die('Unable to load the "PHP Email Form" Library!');
-  }
+if (file_exists($php_email_form = 'php-email-form.php')) {
+  include($php_email_form);
+} else {
+  die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = 'New Form Submission: ' . $_POST['organization'];
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = 'New Form Submission: ' . $_POST['organization'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
@@ -27,15 +27,17 @@
   );
   */
 
-  $contact->add_message($_POST['name'], 'From');
-  $contact->add_message($_POST['email'], 'Email');
-  $contact->add_message($_POST['organization'], 'Organization / Company Name');
-  $contact->add_message($_POST['project_type'], 'Project Type');
-  $contact->add_message($_POST['message'], 'Message', 10);
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['organization'], 'Organization / Company Name');
+$contact->add_message($_POST['project_type'], 'Project Type');
+$contact->add_message($_POST['message'], 'Message', 10);
 
 
 $contact->cc = array('ckiglore3@gmail.com', 'kilgore@dhacontracting.com');
 
-  echo $contact->send();
+echo $contact->send();
+$contact->cc = 'Kilgore@dhacontracting.com';
+
 ?>
 
