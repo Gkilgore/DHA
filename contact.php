60d76d3e6f8051fe1afc78440e6e9a9d8d76bed6
Gkilgore <gkilgore@frhsd.com>
2023-06-14 01:13:18 -0400

    added CT, chancged email thank you. changed subj

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@ $contact->ajax = true;
 $contact->to = $receiving_email_address;
 $contact->from_name = $_POST['name'];
 $contact->from_email = $_POST['email'];
-$contact->subject = 'New Form Submission: ' . $_POST['organization'];
+$contact->subject = 'DHA Website Contact ' . $_POST['organization'];
 
 // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 /*
@@ -25,7 +25,7 @@ $contact->subject = 'New Form Submission: ' . $_POST['organization'];
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  */ 
 
 $contact->add_message($_POST['name'], 'From');
 $contact->add_message($_POST['email'], 'Email');
