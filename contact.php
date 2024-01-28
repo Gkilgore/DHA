d89eb4652d23d6ff713184d3db5dd4cc24c0cafc
Gkilgore <gkilgore@frhsd.com>
2024-01-28 13:17:53 -0500

    chainging HTML setup on contact and php change

--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-
 <?php
 // Replace contact@example.com with your real receiving email address
 $receiving_email_address = 'Dylan@dhacontracting.com';
@@ -25,7 +24,7 @@ $contact->subject = 'DHA Website Contact ' . $_POST['organization'];
     'password' => 'pass',
     'port' => '587'
   );
-  */ 
+  */
 
 $contact->add_message($_POST['name'], 'From');
 $contact->add_message($_POST['email'], 'Email');
@@ -33,11 +32,20 @@ $contact->add_message($_POST['organization'], 'Organization / Company Name');
 $contact->add_message($_POST['project_type'], 'Project Type');
 $contact->add_message($_POST['message'], 'Message', 10);
 
-
 $contact->cc = array('kiglore3@gmail.com', 'kilgore@dhacontracting.com');
 
-echo $contact->send() ;
-$contact->cc = 'Kilgore@dhacontracting.com';
+// Attempt to send the email
+$email_sent = $contact->send();
 
-?>
+// Check if the email was sent successfully
+if ($email_sent) {
+  // Fire the Google Ads tracking tag after a successful email sending operation
+  echo '<script>';
+  echo 'gtag("event", "conversion", {"send_to": "AW-11479996666/el-5CNndjowZEPqxi-Iq"});';
+  echo '</script>';
 
+  // Optionally, you can include additional JavaScript code or perform other actions here
+}
+
+// Return the result of the email sending operation
+echo $email_sent;
